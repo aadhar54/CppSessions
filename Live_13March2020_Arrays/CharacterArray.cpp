6de471cb5af61f0171c3e42cca45320dcabaa785
Aadhar Bupkis Bhatnagar <36945867+aadhar54@users.noreply.github.com>
2020-03-13 13:28:44 +0530

    Add files via upload

---
+++ Live_13March2020_Arrays/CharacterArray.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+char arr[] =  {  'A','B','C' };
+char arr2[] = "ABC" ;
+char arr3[] = "Hello CB";
+char arr4[100];
+char arr5[20] = "Welcome";
+
+int iarr[] = {1,2,3,4} ; 
+char  carr[] = {'a','b','c','\0'};
+
+cout<<iarr<<endl;
+cout<<carr<<endl;
+cout<<arr2<<endl;
+//cout<<sizeof(arr)<<endl;
+//cout<<sizeof(arr2)<<endl;
+//cout<<sizeof(arr3)<<endl;
+//cout<<sizeof(arr4)<<endl;
+//cout<<sizeof(arr5)<<endl;
+
+return 0;
+}
